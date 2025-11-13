4fcb867fe1612c58f2d44217cfdb8d0091f52087
rafaelporfiriobarros <rafaporfirio.barros@gmail.com>
2025-11-13 13:43:48 -0300

    puxando dados de todos os videos do mrbeast

--- video_stats.py
+++ video_stats.py
@@ -59,6 +59,53 @@ def get_video_ids(playlistId):
         raise e
 
 
+def batch_list(video_id_list, batch_size):
+    for video_id in range(0, len(video_id_list), batch_size):
+        yield video_id_list[video_id:video_id + batch_size]
+
+
+def extract_video_data(video_ids):
+    extracted_data = []
+
+    def batch_list(video_id_list, batch_size):
+        for video_id in range(0, len(video_id_list), batch_size):
+            yield video_id_list[video_id:video_id + batch_size]
+
+
+    try:
+        for batch in batch_list(video_ids, maxResults):
+            video_ids_str = ",".join(batch)
+
+            url = f"https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&part=snippet&part=statistics&id={video_ids_str}&key={API_KEY}"
+
+            response = requests.get(url)
+            response.raise_for_status()  # Gera exceção se status != 200
+
+            data = response.json()
+
+            for item in data.get("items", []):
+                video_id = item["id"]
+                snippet = item['snippet']
+                contentDetails = item['contentDetails']
+                statistics = item['statistics']
+
+                video_data = {
+                    "video_id": video_id,
+                    "title": snippet.get("title"),
+                    "publishedAt": snippet.get("publishedAt"),
+                    "duration": contentDetails.get("duration"),
+                    "viewCount": statistics.get("viewCount"),
+                    "likeCount": statistics.get("likeCount"),
+                    "commentCount": statistics.get("commentCount"),
+                }
+
+                extracted_data.append(video_data)   
+        return extracted_data
+    
+    except requests.exceptions.RequestException as e:
+        raise e
+
 if __name__ == "__main__":
     playlistId = get_playlist_id()
-    print(get_video_ids(playlistId))
+    video_ids = get_video_ids(playlistId)   
+    print(extract_video_data(video_ids))
